79f3eeae38961feb5f14bed39ae95c027232617f
mrPechen <lupuss26@gmail.com>
2024-05-21 15:14:27 +0300

    added input validator

--- tgbot/handlers/aggregate.py
+++ tgbot/handlers/aggregate.py
@@ -3,10 +3,13 @@ import json
 from aiogram import Router
 from aiogram.types import Message
 
+from tgbot.services.validators.input_validator import InputData
+
 aggregate_router = Router()
 
 
 @aggregate_router.message()
 async def user_start(message: Message):
     text_to_json = json.loads(message.text)
-    await message.answer(text=str(text_to_json))
+    input_data = InputData(**text_to_json)
+    await message.answer(text=str(input_data))
